b96db1d5323adc7e0a5c5337d8b89ee8c67868df
Ashraf Mohamed <ashraflkj21@gmail.com>
2026-06-07 14:39:09 +0300

    Update (Question,Course,QuestionOption,ModelAnswer)create_tables.sql

--- Tables/(Question,Course,QuestionOption,ModelAnswer)create_tables.sql
+++ Tables/(Question,Course,QuestionOption,ModelAnswer)create_tables.sql
@@ -24,5 +24,5 @@ CREATE TABLE ModelAnswer (
     ModelAnswerID   SERIAL PRIMARY KEY,
     QuestionID      INT NOT NULL REFERENCES Question(QuestionID) ON DELETE CASCADE,
     CorrectOptionID INT NOT NULL REFERENCES Option(OptionID)    ON DELETE CASCADE,
-    UNIQUE (QuestionID)          -- one correct answer per question
+    UNIQUE (QuestionID)          
 );
